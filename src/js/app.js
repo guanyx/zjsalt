42135f1e3897f94e44b47baac75a1b0f677c72ee
guanyx <476848761@163.com>
2016-08-17 11:09:25 +0800

    update

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import '../css/main.scss';
+import './lib/table';
+import './lib/placeholder';
---
+++ src/js/lib/table.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import JM from 'jquery-mousewheel';
+import MCSP from 'malihu-custom-scrollbar-plugin';
+
+JM($);
+MCSP($);
+
+$('.table-table-inner').mCustomScrollbar();
