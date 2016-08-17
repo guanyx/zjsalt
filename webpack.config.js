2de709f756508990948e468ea6127c5128cf8963
guanyx <476848761@163.com>
2016-08-17 22:41:41 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ loaders.push({
 loaders.push([
     { 
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-        loader: "url-loader?limit=10000&minetype=application/font-woff"
+        loader: "url-loader?limit=1&minetype=application/font-woff"
     },
     { 
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
